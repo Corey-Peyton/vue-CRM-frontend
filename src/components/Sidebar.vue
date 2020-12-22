9e8817d804e3d8d09166d27b39a60d0075634bf4
stevensam0109 <steven.sam.0109@outlook.com>
2020-12-22 19:26:43 -0300

    minor change on sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -30,7 +30,7 @@
                     @click="doLogout"
                     class="w-full">
                     <a 
-                        class="block text-center md:text-left px-4 py-2 mt-2 text-sm font-semibold 
+                        class="block text-center sm:text-left px-4 py-2 mt-2 text-sm font-semibold 
                                 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 
                                 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white 
                                 dark-mode:text-gray-200 sm:mt-0 hover:text-gray-900 focus:text-gray-900 
